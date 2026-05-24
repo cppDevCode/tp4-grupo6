bf03f70baf193a2c41412593233f30b7d8a4c7e7
Julian Riedinger <julianriedinger27@gmail.com>
2026-05-24 20:14:46 -0300

    fix: correcion del codigo para seguir las reglas de estilos

--- middlewares/profesor-validacion.middleware.ts
+++ middlewares/profesor-validacion.middleware.ts
@@ -1,39 +1,39 @@
-import { Request, Response, NextFunction } from "express";
+import { Request, Response, NextFunction } from 'express'
 
 export const validarProfesor = (req: Request, res: Response, next: NextFunction) => {
-  const { nombre, apellido, email, dni, cargo, isActive, materias } = req.body;
+  const { nombre, apellido, email, dni, cargo, isActive, materias } = req.body
 
-  const error = [];
+  const error = []
 
-  if (nombre === "" || nombre === undefined || typeof nombre !== "string") {
-    error.push("Ingrese un nombre valido");
+  if (nombre === '' || nombre === undefined || typeof nombre !== 'string') {
+    error.push('Ingrese un nombre valido')
   }
-  if (apellido === "" || apellido === undefined || typeof apellido !== "string") {
-    error.push("Ingrese un apellido valido");
+  if (apellido === '' || apellido === undefined || typeof apellido !== 'string') {
+    error.push('Ingrese un apellido valido')
   }
-  if (email === "" || email === undefined || typeof email !== "string") {
-    error.push("Ingrese un mail valido");
+  if (email === '' || email === undefined || typeof email !== 'string') {
+    error.push('Ingrese un mail valido')
   }
-  if (dni === undefined || typeof dni !== "number") {
-    error.push("Ingrese un dni valido");
+  if (dni === undefined || typeof dni !== 'number') {
+    error.push('Ingrese un dni valido')
   }
-  if (cargo === "" || cargo === undefined || typeof cargo !== "string") {
-    error.push("Ingrese un cargo valido");
+  if (cargo === '' || cargo === undefined || typeof cargo !== 'string') {
+    error.push('Ingrese un cargo valido')
   }
-  if (isActive === undefined || typeof isActive !== "boolean") {
-    error.push("Ingrese un estado valido: verdadero o falso");
+  if (isActive === undefined || typeof isActive !== 'boolean') {
+    error.push('Ingrese un estado valido: verdadero o falso')
   }
   if (
     !Array.isArray(materias) ||
     materias.length === 0 ||
-    materias.some((materia) => typeof materia !== "string")
+    materias.some((materia) => typeof materia !== 'string')
   ) {
-    error.push("Ingrese un array de materias valido");
+    error.push('Ingrese un array de materias valido')
   }
 
   if (error.length > 0) {
-    return res.status(400).json({ msg: "Datos invalidos", error });
+    return res.status(400).json({ msg: 'Datos invalidos', error })
   }
 
-  next();
-};
+  next()
+}
--- models/extras/profesor.model.ts
+++ models/extras/profesor.model.ts
@@ -1,12 +1,12 @@
-import { PersonaModel } from "../persona.model";
+import { PersonaModel } from '../persona.model'
 
 export class ProfesorModel extends PersonaModel {
-  private dni: number;
-  private cargo: string;
-  private fechaAlta: string;
-  private modificacion: string;
-  private materias: string[];
-  private isActive: boolean;
+  private dni: number
+  private cargo: string
+  private fechaAlta: string
+  private modificacion: string
+  private materias: string[]
+  private isActive: boolean
 
   constructor(
     nombre: string,
@@ -16,81 +16,81 @@ export class ProfesorModel extends PersonaModel {
     cargo: string,
     isActive: boolean,
     materias: string[],
-    fechaAlta: string = new Date().toISOString().split("T")[0],
-    modificacion: string = new Date().toISOString().split("T")[0],
+    fechaAlta: string = new Date().toISOString().split('T')[0],
+    modificacion: string = new Date().toISOString().split('T')[0]
   ) {
-    super(nombre, apellido, email);
-    this.dni = dni;
-    this.cargo = cargo;
-    this.fechaAlta = fechaAlta;
-    this.modificacion = modificacion;
-    this.isActive = isActive;
-    this.materias = materias;
+    super(nombre, apellido, email)
+    this.dni = dni
+    this.cargo = cargo
+    this.fechaAlta = fechaAlta
+    this.modificacion = modificacion
+    this.isActive = isActive
+    this.materias = materias
   }
 
   //dni
   setDni(dni: number): void {
-    this.dni = dni;
+    this.dni = dni
   }
 
   getDni(): number {
-    return this.dni;
+    return this.dni
   }
 
   //cargo
   setCargo(cargo: string): void {
-    this.cargo = cargo;
+    this.cargo = cargo
   }
 
   getCargo(): string {
-    return this.cargo;
+    return this.cargo
   }
 
   //fechaAlta
   setFechaAlta(fechaAlta: string): void {
-    this.fechaAlta = fechaAlta;
+    this.fechaAlta = fechaAlta
   }
 
   getFechaAlta(): string {
-    return this.fechaAlta;
+    return this.fechaAlta
   }
 
   //modificacion
   setModificacion(modificacion: string): void {
-    this.modificacion = modificacion;
+    this.modificacion = modificacion
   }
 
   getModificacion(): string {
-    return this.modificacion;
+    return this.modificacion
   }
 
   //isActive
   setIsActive(isActive: boolean): void {
-    this.isActive = isActive;
+    this.isActive = isActive
   }
 
   getIsActive(): boolean {
-    return this.isActive;
+    return this.isActive
   }
 
   //materias
   setMaterias(materias: string[]): void {
-    this.materias = materias;
+    this.materias = materias
   }
 
   getMaterias(): string[] {
-    return this.materias;
+    return this.materias
   }
 
   setProfesor(profesor: ProfesorModel): void {
-    this.nombre = profesor.getNombre();
-    this.apellido = profesor.getApellido();
-    this.email = profesor.getEmail();
-    this.dni = profesor.getDni();
-    this.cargo = profesor.getCargo();
-    this.fechaAlta = profesor.getFechaAlta();
-    this.modificacion = profesor.getModificacion();
-    this.materias = profesor.getMaterias();
+    this.nombre = profesor.getNombre()
+    this.apellido = profesor.getApellido()
+    this.email = profesor.getEmail()
+    this.dni = profesor.getDni()
+    this.cargo = profesor.getCargo()
+    this.fechaAlta = profesor.getFechaAlta()
+    this.modificacion = profesor.getModificacion()
+    this.materias = profesor.getMaterias()
   }
 
   public getAllAttributes(): object {
@@ -100,7 +100,7 @@ export class ProfesorModel extends PersonaModel {
       cargo: this.cargo,
       fechaAlta: this.fechaAlta,
       modificacion: this.modificacion,
-      materias: this.materias,
-    };
+      materias: this.materias
+    }
   }
 }
